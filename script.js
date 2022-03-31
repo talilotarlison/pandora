2c9ca4b702ab54fc6d773ea129d20c4774590088
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2022-03-31 15:57:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var myMusicInputID = myMusicInput.substr(17)
 
 var linkMusicNaTela = document.getElementById("video");
 var linkEmbedID = "https://www.youtube.com/embed/" + myMusicInputID
-var linkIframe = '<iframe width="670" height="640" src="' +linkEmbedID+ '" title="player" frameborder="0" allow="autoplay; clipboard-write;"></iframe>'
+var linkIframe = '<iframe width="670" height="650" src="' +linkEmbedID+ '" title="player" frameborder="0" allow="autoplay; clipboard-write;"></iframe>'
 var linkArtista = '<h1><font color="white">Pandora Radio</font><h1><h2><font color="white">Reproduzindo agora</font></h2>'
 var linkFinal = linkIframe + linkArtista
 
